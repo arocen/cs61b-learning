069efd9ddc6a7df539b44916e65f9f446fe62efd
arocen <aufger1913@hotmail.com>
2023-11-26 00:10:14 +0800

    modified style

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -4,8 +4,9 @@ public class ArrayDeque<T> {
     private T[] list;
     private int size;
     /** Create an empty array list. */
+    public static final int MIN_SIZE = 16;
     public ArrayDeque() {
-        list = (T []) new Object[8];
+        list = (T[]) new Object[MIN_SIZE];
         size = 0;
     }
     public int size() {
@@ -29,15 +30,12 @@ public class ArrayDeque<T> {
         size++;
     }
     private void resize(int newLength) {
-        T[] newList = (T []) new Object[newLength];
+        T[] newList = (T[]) new Object[newLength];
         System.arraycopy(list, 0, newList, 0, size);
         list = newList;
     }
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        }
-        return false;
+        return size == 0;
     }
     public void printDeque() {
         for (int i = 0; i < size; i++) {
@@ -58,7 +56,7 @@ public class ArrayDeque<T> {
         }
         list[size - 1] = null;
         size--;
-        if (size < 0.25 * list.length && list.length >= 16) {
+        if (size < 0.25 * list.length && list.length >= MIN_SIZE) {
             resize(list.length / 2); // list.length is always even.
         }
         return removed;
@@ -70,7 +68,7 @@ public class ArrayDeque<T> {
         T removed = list[size - 1];
         list[size - 1] = null;
         size--;
-        if (size < 0.25 * list.length && list.length >= 16) {
+        if (size < 0.25 * list.length && list.length >= MIN_SIZE) {
             resize(list.length / 2); // list.length is always even.
         }
         return removed;
