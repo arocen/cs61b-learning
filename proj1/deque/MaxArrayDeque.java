0ff97aba0f3b35808e8afc24d5a7e5370b53652b
arocen <aufger1913@hotmail.com>
2023-12-10 16:48:06 +0800

    completed MaxArrayDeque and started writing test code

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -8,6 +8,33 @@ public class MaxArrayDeque<T> extends ArrayDeque<T>{
         super();
         cmp = c;
     }
-    public T max() {}
-    public T max(Comparator<T> c) {}
+    public T max() {
+        if (this.size() == 0) {
+            return null;
+        }
+        // Loop and compare to get maximum.
+        int maxIndex = 0;
+        int currentIndex = 0;
+        while (currentIndex < this.size()) {
+            if (cmp.compare(this.get(maxIndex), this.get(currentIndex)) < 0) {
+                maxIndex = currentIndex;
+            }
+            currentIndex++;
+        }
+        return this.get(maxIndex);
+    }
+    public T max(Comparator<T> c) {
+        if (this.size() == 0) {
+            return null;
+        }
+        int maxIndex = 0;
+        int currentIndex = 0;
+        while (currentIndex < this.size()) {
+            if (c.compare(this.get(maxIndex), this.get(currentIndex)) < 0) {
+                maxIndex = currentIndex;
+            }
+            currentIndex++;
+        }
+        return this.get(maxIndex);
+    }
 }
---
+++ proj1/deque/MaxArrayDequeTest.java
@@ -0,0 +1,16 @@
+package deque;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.junit.Assert.*;
+
+public class MaxArrayDequeTest {
+    /** ComparatorA used to test MaxArrayDeque. */
+    private class ComparatorA implements Comparator {
+        @Override
+        ComparatorA
+
+    }
+}
