f056bc415cfc18c786d51430ec9697da144855f2
arocen <aufger1913@hotmail.com>
2023-12-10 16:12:03 +0800

    completed launcher of MaxArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -5,10 +5,8 @@ import java.util.Comparator;
 public class MaxArrayDeque<T> extends ArrayDeque<T>{
     public Comparator cmp;
     public MaxArrayDeque(Comparator<T> c) {
-        cmp = new Comparator<T>();
-    }
-    private class defaultComparator{
-
+        super();
+        cmp = c;
     }
     public T max() {}
     public T max(Comparator<T> c) {}
